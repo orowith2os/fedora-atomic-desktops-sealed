7f6d6d8ea7e58d1187cc553b613b801aa50f0d9c
Dallas Strouse <dallas.strouse2007@gmail.com>
2026-06-20 01:57:18 -0500

    scripts/bootc-container-lint: Skip some lints for Bazzite
    Bazzite images don't pass right now, we'll fix this later.

--- scripts/bootc-container-lint.sh
+++ scripts/bootc-container-lint.sh
@@ -12,4 +12,9 @@ if [[ "${NAME}" == "secureblue" ]]; then
     args+=("--skip" "nonempty-run-tmp")
 fi
 
+if [[ "${NAME}" == "Bazzite" ]]; then
+    args+=("--skip" "nonempty-boot"
+           "--skip" "sysusers")
+fi
+
 bootc container lint --fatal-warnings --no-truncate "${args[@]}"
