ce59c0d67914b47c991f4e1ee85bc7d4cc7aab17
Timothée Ravier <tim@siosm.fr>
2026-07-10 17:48:01 +0200

    scripts/prepare-rootfs: Only install latest bootc from copr for bazzite

--- scripts/prepare-rootfs.sh
+++ scripts/prepare-rootfs.sh
@@ -20,11 +20,17 @@ dnf remove -y \
     gnome-software-rpm-ostree \
     plasma-discover-rpm-ostree
 
-# Temporarily pull bootc from the rhcontainerbot copr to get the latest
-# fixes for Bazzite: https://copr.fedorainfracloud.org/coprs/rhcontainerbot/bootc/
-dnf install -y 'dnf5-command(copr)'
-dnf copr enable -y rhcontainerbot/bootc "fedora-$(source /etc/os-release && echo "$VERSION_ID")-x86_64"
-dnf upgrade -y --refresh bootc
+
+NAME="$(source /etc/os-release && echo "${NAME}")"
+if [[ "${NAME}" == "Bazzite" ]]; then
+    # Temporarily pull bootc from the rhcontainerbot copr to get the latest
+    # fixes for Bazzite: https://copr.fedorainfracloud.org/coprs/rhcontainerbot/bootc/
+    dnf install -y 'dnf5-command(copr)'
+    dnf copr enable -y rhcontainerbot/bootc "fedora-$(source /etc/os-release && echo "$VERSION_ID")-x86_64"
+else
+    # Install latest bootc release
+    dnf upgrade -y --enablerepo=updates-testing --refresh bootc
+fi
 
 # Uninstall bootupd (no support for systemd-boot yet)
 rpm -e bootupd
